de87077c064255b175436b366b0de9927738eea1
Arif khan <arif0852147@gmail.com>
2025-06-15 17:07:14 +0500

    Just for practice

---
+++ lib/prectice.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class Prectice extends StatefulWidget {
+  const Prectice({super.key});
+
+  @override
+  State<Prectice> createState() => _PrecticeState();
+}
+
+class _PrecticeState extends State<Prectice> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
